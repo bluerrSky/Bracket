9acf0827d154849abc4ba3bf419a4216b8358ba9
bluerrSky <rocin158ante@gmail.com>
2025-11-01 12:51:42 +0530

    changes for vercel 3

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const IS_PROD = app.get('env') === 'production';
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://bracket.vercel.app'], 
+    origin: ['http://localhost:5173', "https://bracket-eight.vercel.app"], 
     credentials: true
 }));
 
